90f5b00b8955678ccfc614735dfa7be23415c5a5
KevinLi19 <kevin.li@georgiancollege.ca>
2024-09-25 19:43:04 -0400

    Added PhoneNumber TextBox

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.week4stringlabregex {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.week4stringlabregex to javafx.fxml;
+    exports com.example.week4stringlabregex;
+}
